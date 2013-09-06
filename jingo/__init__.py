8e74f8a8e94e9aa78a93db8da10cba98729aee37
Aldiantoro Nugroho <kriwil@gmail.com>
2013-09-06 10:54:36 +0700

    Add get_template_sources. Used by compress_jingo management command.

--- jingo/__init__.py
+++ jingo/__init__.py
@@ -216,6 +216,24 @@ class Loader(BaseLoader):
 
         return True
 
+    def get_template_sources(self, template_name, template_dirs=None):
+        """
+        Returns the absolute paths to "template_name", when appended to each
+        directory in "template_dirs". Any paths that don't lie inside one of the
+        template dirs are excluded from the result set, for security reasons.
+        """
+        if not template_dirs:
+            template_dirs = []
+        for template_dir in template_dirs:
+            try:
+                yield safe_join(template_dir, template_name)
+            except UnicodeDecodeError:
+                # The template dir name was a bytestring that wasn't valid UTF-8.
+                raise
+            except ValueError:
+                # The joined path was located outside of template_dir.
+                pass
+
     def load_template(self, template_name, template_dirs=None):
         if not self._valid_template(template_name):
             raise TemplateDoesNotExist(template_name)
